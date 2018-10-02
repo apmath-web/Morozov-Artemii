c9942062c7c0e8d0618e3741eff689bc5cbc7bfb
morozzArt <33790882+morozzArt@users.noreply.github.com>
2018-10-03 00:55:53 +0300

    Delet comments

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-	"log"      // пакет для логирования
-	"net/http" // пакет для поддержки HTTP протокола
+	"log"      
+	"net/http"
 	"simple_HTTP_server/handlers"
 )
 
 
 
 func main() {
-	http.HandleFunc("/date", handlers.Date) // установим роутер
+	http.HandleFunc("/date", handlers.Date) 
 	http.HandleFunc("/wait", handlers.Wait)
-	err := http.ListenAndServe(":9000", nil) // задаем слушать порт
+	err := http.ListenAndServe(":9000", nil) 
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
